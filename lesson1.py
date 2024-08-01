7ab344822d5ca2e2b23c50d8dc1606306ff1bbdd
MilkaMika <themikamilchenko19@gmail.com>
2024-08-01 19:04:42 +0300

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,10 @@
+print(9**0.5*5) #1st program
+print(99.9>99.8 and 1000<1000.1) #2nd program
+print(1234//10) #3rd program
+print(123%100)
+print(5678//10)
+print(567%100)
+print(23+67)
+print(int(13.42))
+print(int(42.13))
+print(13==13 and 42==42)
---
+++ lesson2.py
@@ -0,0 +1,21 @@
+name = "Топинамбур"
+print(name[0])
+print(name[-1])
+print(name[5 : 10])
+print(name[::-1])
+print(name[1:10])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
